4b33b740a239aefdf85a91e3b25fb5a108c3dfd6
John Durham <jdurham2843@gmail.com>
2018-08-11 23:13:08 -0500

    reformat

--- main.py
+++ main.py
@@ -5,19 +5,7 @@ MIN, MAX = -1, 1
 THRESHOLD = 2
 DISTANCE = abs(MIN) + MAX
 
-def f(val: float):
-    return (val * val) - complex(0.221, 0.713)
-
-def pixel_start_value_x(i: int) -> float:
-    return MIN + ((i * DISTANCE) / WIDTH)
-
-def pixel_start_value_y(i: int) -> float:
-    return MAX - ((i * DISTANCE) / HEIGHT)
-
-image = Image.new('L', (WIDTH, HEIGHT))
-pix = image.load()
-
-def generate():
+def generate(pix):
     for wi in range(0, WIDTH):
         real = pixel_start_value_x(wi)
         for yi in range(0, HEIGHT):
@@ -29,9 +17,19 @@ def generate():
                 count += 1
             pix[wi, yi] = count
 
+def pixel_start_value_x(i: int) -> float:
+    return MIN + ((i * DISTANCE) / WIDTH)
+
+def pixel_start_value_y(i: int) -> float:
+    return MAX - ((i * DISTANCE) / HEIGHT)
+
+def f(val: float):
+    return (val * val) - complex(0.221, 0.713)
 
 if __name__ == '__main__':
-    generate()
+    image = Image.new('L', (WIDTH, HEIGHT))
+    pix = image.load()
+    generate(pix)
     image.save('fractal.png')
         
 
